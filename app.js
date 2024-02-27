7981388acb0c6994bf82ef55e3d4eee79bbab3dd
muhammad-danish <totalnetsolution2@gmail.com>
2024-02-28 01:44:45 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,13 @@
+var age = ("My age is 40 years old")
+
+alert(age)
+
+var visits=0;
+visits++;
+alert("You have visited this site "+visits+" times");
+
+
+var birthYear = 1981;
+var browserMessage = ("My birth year is " + birthYear + "<br /> Data type of my declared variable is number");
+document.write(browserMessage);
+
